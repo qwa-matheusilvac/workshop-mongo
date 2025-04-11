32c5200e5d983dbbdf49ad6a0895fbff3149af59
qwa-matheusilvac <matheus.correia@qwasolucoes.com.br>
2025-04-11 11:38:53 -0300

    project finish

--- src/main/java/github/matheusilvac/workshopmongo/doman/persistence/repository/PostRepository.java
+++ src/main/java/github/matheusilvac/workshopmongo/doman/persistence/repository/PostRepository.java
@@ -4,8 +4,15 @@ import github.matheusilvac.workshopmongo.doman.persistence.entity.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.sql.Date;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    //@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
     Page<Post> findByTitleContainsIgnoreCase(String text, Pageable pageable);
+
+//    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+//    Page<Post> fullSearch(String text, Date minDate, Date maxDate, Pageable pageable);
 }
